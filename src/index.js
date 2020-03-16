128a1a7220fe87da8d50507c89013e7c6366d071
edkondr <60880285+edkondr@users.noreply.github.com>
2020-03-17 00:00:53 +0300

    feat:add files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort(matrix) {
-  if (arguments.length === 0) return [];
-  let someArr = [];
-  matrix.forEach((item, index) => {
-      someArr.push((index % 2 ? (item = item.reverse()) : (item = item)));
-  });
-  return someArr.flat();
+module.exports = function towelSort (array) {
+  if (!array || array.length == 0){
+    return [];
+  }
+  for(let i = 1; i<array.length; i=i+2) {
+    array[i].sort((a,b) => b-a);//стрелочная функция
+  }
+  return array.flat();
 }
