7722fbfee2e71394f84f414537b5f61cb9817a61
edkondr <edyard2002@mail.ru>
2020-10-25 19:37:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,5 +8,5 @@ module.exports = function towelSort (array) {
   for(let i = 1; i<array.length; i=i+2) {//сортировка массива четные меняем местами
     array[i].sort((a,b) => b-a);
   }
-  return array.flat();//возвращаем новый массив с подмасивами
+  return array.flat();//возвращаем новый массив с подмассивами
 }
